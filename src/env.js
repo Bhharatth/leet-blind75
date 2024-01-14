ab257a28de1e3736144d7fc68b3db268fb4e6a61
bharath <bharathprasannan96@gmail.com>
2024-01-14 12:05:27 +0530

    env variable added on env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,8 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.development.local' });
+
 
 export const env = createEnv({
   /**
@@ -28,8 +31,13 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url()
     ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    // DISCORD_CLIENT_ID: z.string(),
+    // DISCORD_CLIENT_SECRET: z.string(),
+      POSTGRES_URL:z.string(),
+      POSTGRES_PRISMA_URL:z.string(),
+      POSTGRES_URL_NON_POOLING:z.string(),
+      POSTGRES_HOST:z.string(),
+      POSTGRES_DATABASE:z.string(),
   },
 
   /**
@@ -50,8 +58,13 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    POSTGRES_URL:process.env.POSTGRES_URL,
+    POSTGRES_PRISMA_URL:process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NON_POOLING:process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_HOST:process.env.OSTGRES_HOST,
+    POSTGRES_DATABASE:process.env.POSTGRES_DATABASE,
+    // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
